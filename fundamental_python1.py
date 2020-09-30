6256e59549fa6bda7804197bc57ec7ef1a9c8be5
josetya <jonisetyawan@gmail.com>
2020-09-30 12:43:23 +0700

    fundamental python

---
+++ fundamental_python1.py
@@ -0,0 +1,19 @@
+# konstruksi dasar pytho
+# squential
+print("Hello World!")
+print("by Joni")
+print('Tanggal 30 september 2020')
+print('-' * 8)
+
+#percabangan eksekusi terpilih if else
+ingin_cepat = False
+if ingin_cepat:
+    print('jalan terus')
+else:
+    print('cari jalan lain')
+
+#perulangan for range dan function f
+jumlah_anak = 4
+
+for indeks_anak in range(1, jumlah_anak+1):
+    print(f'hallo anak = {indeks_anak}')
--- main.py
+++
@@ -1,6 +0,0 @@
-# konstruksi dasar pytho
-# squential
-print("Hello World!")
-print("by Joni")
-print('Tanggal 30 september 2020')
-print('-' * 8)
